8140c7cced60c39d258951a6bc221c651402ccad
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-07-12 23:10:55 +0100

    style: reduce max width of logo

--- app/App.tsx
+++ app/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <main className="flex flex-col min-h-screen bg-white px-4 py-8">
 
       <div className="flex-1 flex flex-col items-center justify-center">
-        <img src="/app/assets/logo-min.png" className="text-9xl mb-3 md:max-w-[768px]" alt="City Guides"></img>
+        <img src="/app/assets/logo-min.png" className="text-9xl mb-3 md:max-w-[600px]" alt="City Guides"></img>
         <h1 className="text-4xl font-serif font-bold text-gray-700">City Guides</h1>
       </div>
 
