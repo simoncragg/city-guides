e9e3e1e075e7c35a953338f411f30a609ff05827
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-08-03 13:44:29 +0100

    refactor: extract Logo component

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,11 @@
 import Chat from "./components/Chat";
+import Logo from "./components/Logo";
 
 function App() {
 
   return (
     <main className="flex flex-col min-h-screen bg-white px-2 py-8">
-
-      <div className="flex-1 flex flex-col items-center justify-center">
-        <img src={`${import.meta.env.VITE_PUBLIC_URL}/logo-min.png`} className="text-9xl mb-3 md:max-w-[600px]" alt="City Guides" />
-        <h1 className="text-4xl font-serif font-bold text-gray-700">City Guides</h1>
-      </div>
-
+      <Logo customClasses="flex-1 justify-center" />
       <div className="flex-1 w-full max-w-screen-lg mx-auto">
         <Chat />
       </div>
---
+++ app/components/Logo.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+interface LogoProps {
+  customClasses?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ customClasses = "" }) => {
+  return (
+    <div className={`flex flex-col items-center ${customClasses}`}>
+      <img 
+        src={`${import.meta.env.VITE_PUBLIC_URL}/logo-min.png`} 
+        className="text-9xl mb-3 md:max-w-[600px]"
+        alt="City Guides"
+      />
+      <h1 className="text-4xl font-serif font-bold text-gray-700">
+        City Guides
+      </h1>
+    </div>
+  );
+}
+
+export default Logo;
