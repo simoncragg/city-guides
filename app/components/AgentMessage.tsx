6b15124c08aa94f775b81bf74707ab34bd27551f
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-08-03 22:22:40 +0100

    style: increase h3 text size

--- app/components/AgentMessage.tsx
+++ app/components/AgentMessage.tsx
@@ -31,7 +31,7 @@ const AgentMessage: React.FC<Props> = ({ message, lastAgent }) => {
             className={`
               [&_ul]:mb-4 [&_ol]:mb-4
               [&_ul>li]:mt-4 [&_ol>li]:mt-4
-              [&_h3]:text-base [&_h3]:font-semibold [&_h3]:mt-4 [&_h3]:mb-2.5
+              [&_h3]:text-xl [&_h3]:font-semibold [&_h3]:mt-4 [&_h3]:mb-2.5
               [&_p]:inline
             [&_a]:text-sky-800
               [&_img]:rounded-xl [&_img]:my-4
