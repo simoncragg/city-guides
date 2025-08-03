09b9bcbd8f2a215c6e2dfbaf51237af81b472728
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-08-03 11:58:01 +0100

    feat: add error tracing to getPhotoUriAsync

--- api/services/photos.ts
+++ api/services/photos.ts
@@ -7,22 +7,34 @@ export async function getPhotoUriAsync(photoName: string): Promise<string | null
     `${apiBaseUrl}/${photoName}/media` +
     `?maxHeightPx=400&maxWidthPx=400&skipHttpRedirect=true`;
 
-  const res = await fetch(mediaUrl, {
-    headers: {
-      "X-Goog-Api-Key": apiKey,
-      "X-Goog-FieldMask": "photoUri",
-    },
-  });
+  console.log("DEBUG 1");
 
-  if (!res.ok) {
-    const msg = "Image retrieval failed";
-    console.error(msg, await res.text());
-    throw new Error(`${msg}: ${res.status}`);
+  try {
+    const res = await fetch(mediaUrl, {
+      headers: {
+        "X-Goog-Api-Key": apiKey,
+        "X-Goog-FieldMask": "photoUri",
+      },
+    });
+
+    console.log("DEBUG 2");
+
+    if (!res.ok) {
+      const msg = "Image retrieval failed";
+      console.error(msg, await res.text());
+      throw new Error(`${msg}: ${res.status}`);
+    }
+
+    const { photoUri } = await res.json() as { photoUri: string };
+
+    return photoUri
+      ? photoUri
+      : null;
   }
-
-  const { photoUri } = await res.json() as { photoUri: string };
-
-  return photoUri
-    ? photoUri
-    : null;
+  catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error("Photos error: " + err.message);
+    }
+    throw err;
+  }
 }
